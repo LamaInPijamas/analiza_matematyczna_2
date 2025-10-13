1c95c54a292dfa07c852c43497192bc1f9761955
daynlight <danielstodulski000@gmail.com>
2025-10-13 21:57:35 +0100

    Base OpenGL renderer

--- Lab2 Krzywe zioma/Renderer/OpenGL/OpenGL_Renderer.cpp
+++ Lab2 Krzywe zioma/Renderer/OpenGL/OpenGL_Renderer.cpp
@@ -24,7 +24,10 @@ void OpenGLRenderer::Renderer::createWindow()
     throw std::runtime_error("Can't initialize GLFW");
   }
 
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
 
   window = glfwCreateWindow(800, 600, "Curve", nullptr, nullptr);
   if(!window) {
@@ -40,7 +43,17 @@ void OpenGLRenderer::Renderer::createWindow()
 
 void OpenGLRenderer::Renderer::createRenderer()
 {
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    running = false;
+    return;
+  };
 
+  int width, height;
+  glfwGetFramebufferSize(window, &width, &height);
+  glViewport(0, 0, width, height);
+
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
 }
 
 bool OpenGLRenderer::Renderer::isRunning()
@@ -49,7 +62,9 @@ bool OpenGLRenderer::Renderer::isRunning()
 }
 
 void OpenGLRenderer::Renderer::renderFrame() {
+  glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
-
+  
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
   glfwSwapBuffers(window);
 };
--- Lab2 Krzywe zioma/Renderer/OpenGL/OpenGL_Renderer.h
+++ Lab2 Krzywe zioma/Renderer/OpenGL/OpenGL_Renderer.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "GL/gl.h"
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include <string>
 #include <vector>
